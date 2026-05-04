b6f68a08b1aa003319e3a6c98b865b98ad0f3a3e
Moawad Ibrahim <mo3wadibrahim@gmail.com>
2026-05-04 06:22:06 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   index.html
    	modified:   src/components/GameBoard.jsx
    	modified:   src/index.css

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,20 +1,23 @@
 function GameBoard({ board, onSelectSquare, activePlayer }) {
-   return (
-      <ol id="game-board">
-         {board.map((row, rowIndex) => (
-            <li key={rowIndex}>
-               <ol>
-                  {row.map((playerSymbol, colIndex) => (
-                     <li key={colIndex}>
-                        <button onClick={() => onSelectSquare(colIndex, rowIndex)} disabled={(playerSymbol !== null || activePlayer === 'O')}>
-                           {playerSymbol}
-                        </button>
-                     </li>
-                  ))}
-               </ol>
-            </li>
-         ))}
-      </ol>
-   );
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(colIndex, rowIndex)}
+                  disabled={playerSymbol !== null || activePlayer === "O"}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
 export default GameBoard;
